8ceef9ecea0659aba1ec5fcd8d5141f6ed27c981
Bhawan Sidhu <55447886+bhawansidhu@users.noreply.github.com>
2020-02-07 19:02:11 -0500

    add code

--- server.js
+++ server.js
@@ -1,86 +1,41 @@
 const express = require('express')
-
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+
+//This allows express to make my static content avialable from the public
+app.use(express.static('public'));
+
+//This tells Express to set or register Handlebars as its' Template/View Engine
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-
-const app = express()
-app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.render("homepage", {
     title: "Home",
     headingInfo: "HOME PAGE"
   })
-})
+});
 
 app.get('/register', function (req, res) {
   res.render("register", {
     title: "User Registration",
     headingInfo: "register now",
   })
-})
-
-app.get('/RoomList', function (req, res) {
-  const places = [];
-  places.push(
-    {
-      imgURL: `/image/cozy house.jpg`,
-      title: `One Large Bedroom Apartment`,
-      price: `CAD $150 avg/night`
-    });
+});
 
 
-  places.push(
-    {
-      imgURL: `/image/bedroom.jpg`,
-      title: ` One Beautiful and Large bedroom `,
-      price: `CAD $120 avg/night`
+app.get("/room-listing",(req,res)=>{
 
-    });
-
-
-
-  places.push(
-    {
-      imgURL: `/image/house.jpg`,
-      title: ` Luxurious One Bedroom Apartment`,
-      price: `CAD $200 avg/night`
-    });
-
-
-
-  places.push(
-    {
-      imgURL: `/image/big apartment.jpg`,
-      title: `Large and Beautiful One Bedroom Apartment`,
-      price: `CAD $240 avg/night`
-    });
-
-
-  places.push(
-    {
-      imgURL: `/image/room.jpg.jpg`,
-      title: `Luxurious Master Bedroom `,
-      price: `CAD $265 avg/night`
-
-    });
-
-  places.push(
-    {
-      
-      imgURL: `/image/new room.jpg`,
-      title: `Newly Renovted One Bedroom `,
-      price: `CAD $240 avg/night`
-
-    });
-
-  res.render("RoomList", {
-    title: "List of all Rooms",
-    headingInfo: "All rooms are avilable for rent.",
-    rooms: places
+  res.render("roomListing",{
+      title: "Room Lsting",
+      headingInfo : "Room Lsting Page",
   })
-})
+
+  });
+
 
 
 
